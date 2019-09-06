87890d2ccea9c5f96a3c98793d048ec75073390a
stealmelon <51431688+stealmelon@users.noreply.github.com>
2019-09-06 09:35:12 +0800

    Update main.swift

--- main.swift
+++ main.swift
@@ -6,17 +6,25 @@
 //  Copyright © 2019 imnu. All rights reserved.
 //
 
-print("请输入用户名：")
-var name = readLine()!
+//
+//  main.swift
+//  1
+//
+//  Created by s20181106115 on 2019/9/4.
+//  Copyright © 2019 imnu. All rights reserved.
+//
+
+/*print("请输入用户名：")
+var username = readLine()!
 print("请输入密码：")
-var key = readLine()!
-if(name == "admin" && key == "888888")
+var password = readLine()!
+/*if(username == "admin" && password == "888888")
 {
     print("密码正确！")
 }
 else
 {
-    if(name != "admin")
+    if(username != "admin")
     {
         print("用户不存在！")
     }
@@ -24,5 +32,37 @@ else
     {
         print("密码错误！")
     }
+}*/
+
+if username == "admin"
+{
+    if password == "888888"
+    {
+        print ("密码正确！")
+    }
+    else
+    {
+        print ("密码错误！")
+    }
+}
+else
+{
+    print ("用户名不存在！")
+}*/
+print("请输入您的分数：")
+var Ptr = readLine()!
+var index = Int(Ptr)!
+
+switch index/10 {
+case 10,9:
+    print ("A")
+case 8:
+    print ("B")
+case 7:
+    print ("C")
+case 6:
+    print ("D")
+default:
+    print ("不及格")
 }
 
