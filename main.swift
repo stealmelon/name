df6185204c46391bed6c5c9ea93d48954c1cf728
stealmelon <51431688+stealmelon@users.noreply.github.com>
2019-09-11 11:19:02 +0800

    Add files via upload

--- main.swift
+++ main.swift
@@ -6,14 +6,6 @@
 //  Copyright © 2019 imnu. All rights reserved.
 //
 
-//
-//  main.swift
-//  1
-//
-//  Created by s20181106115 on 2019/9/4.
-//  Copyright © 2019 imnu. All rights reserved.
-//
-
 /*print("请输入用户名：")
 var username = readLine()!
 print("请输入密码：")
@@ -49,7 +41,7 @@ else
 {
     print ("用户名不存在！")
 }*/
-print("请输入您的分数：")
+/*print("请输入您的分数：")
 var Ptr = readLine()!
 var index = Int(Ptr)!
 
@@ -65,4 +57,35 @@ case 6:
 default:
     print ("不及格")
 }
+var i =  1
+var sum = 0
+var n = 99
+while i < 50
+{
+    sum = sum + n + i
+    i += 1
+    n -= 1
+}
+print("Sum = ",sum+50+100)
+
+ 1...99
+ 2...98
+ 49...51
+50 100
+ */
+var numPtr = ""
+var num = 0
+repeat
+{
+    print("请输入一个数字：")
+    numPtr = readLine()!
+    if numPtr != "q"
+    {
+        num = Int(numPtr)!
+        print("您输入数字的二倍为：",num*2)
+    }
+    
+    
+}while numPtr != "q"
+print("再见！")
 
