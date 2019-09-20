e4aedfab27f729d218b40556b4955d0032cb9db7
stealmelon <51431688+stealmelon@users.noreply.github.com>
2019-09-20 09:24:32 +0800

    Update main.swift

--- main.swift
+++ main.swift
@@ -72,7 +72,7 @@ print("Sum = ",sum+50+100)
  2...98
  49...51
 50 100
- */
+ 
 var numPtr = ""
 var num = 0
 repeat
@@ -88,4 +88,90 @@ repeat
     
 }while numPtr != "q"
 print("再见！")
+*/
+//
+//  main.swift
+//  2
+//
+//  Created by s20181106115 on 2019/9/18.
+//  Copyright © 2019 imnu. All rights reserved.
+//
+/*var someInts:[Int] = [10,20,30,40,50]
+
+for item in someInts{
+    print(item)
+}
+var varA = 20
+let constA = 100
+var varC:Float = 20.0
+
+var stringA = "\(varA) * \(constA) = \(varC * 100)"
+print(stringA)
+
+var someInts:[Int] = [10,20,30,40,50]
+var sum = 0
+for item in someInts{
+    sum += item
+}
+print(sum)
+var array:[Int] = [2,4,5,10,46,13,6,7,9]
+var Max = 0
+for item in array{
+    if(item > Max){
+        Max = item
+    }
+}
+print("Max = \(Max)")
+var array = [Int]()
+
+array.append(10)
+array.append(20)
+array += [9]
+
+for item in array{
+    print(item)
+}
+var strs = ["3","a","8","haha","100","hello"]
+//           0   1   2    3      4      5
+var len = strs.count
+var i = 0
+var t:String
+var cnt:Int = 0
+while i < len - 1{
+    t = strs[i]
+    strs[i] = strs[len - 1]
+    strs[len - 1] = t
+    i += 1
+    len -= 1
+    cnt += 1
+}
+for item in strs{
+    print(item)
+}
+print(cnt)
+
+var strs = ["3","a","8","haha","100","hello"]
+for (index, item) in strs.enumerated(){
+    if index < strs.count/2{
+        var temp = ""
+        temp = strs[index]
+        strs[index] = strs[strs.count - index - 1]
+        strs[strs.count - index - 1] = temp
+    }
+}*/
+var arr = [3,5,8,7]
+ //        3 8 5 3
+var i = 0
+var t:Int
+var f = arr[0]
+// 0 3    1 0    2 1    3 2
+for (index, temp) in arr.enumerated(){
+    if index < arr.count - 1{
+        arr[index] = arr[index + 1]
+    }
+}
+arr[arr.count - 1] = f
+for item in arr{
+    print(item)
+}
 
